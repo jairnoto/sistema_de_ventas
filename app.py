9cc70ded976fc1435e385f97772018fb6a3c797a
jairnoto <jaircito002@gmail.com>
2023-11-14 00:24:45 -0400

    Creacion: modulo_principalapp

---
+++ app.py
@@ -0,0 +1,6 @@
+from frontend.views import run
+
+# Aquí puedes realizar cualquier inicialización adicional que necesites antes de iniciar la interfaz de Streamlit
+
+if __name__ == '_main_':
+    run()
