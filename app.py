6cd59b4bd339e6af27403d3a9769658e507d6c47
jairnoto <jaircito002@gmail.com>
2023-11-14 00:36:17 -0400

    Actualizacion de app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from frontend.views import run
 
 # Aquí puedes realizar cualquier inicialización adicional que necesites antes de iniciar la interfaz de Streamlit
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     run()
